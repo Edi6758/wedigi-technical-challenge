97d7a4693edbafbdaf7c04943cc3cd6df685cb51
Edi6758 <ediarts7@gmail.com>
2023-01-20 00:19:40 -0300

    :wrench: finish structure from project
    finish

---
+++ src/components/Shelf/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shelf = () => {
+    return (
+        <div>
+            <h1>vitrine</h1>
+        </div>
+    )
+}
+
+export default Shelf
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Shelf from '../../components/Shelf'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Shelf/>
+        </div>
+    )
+}
+
+export { Home }
